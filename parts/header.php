24ba5bb7e9033a98cd8d4b0e01e250ee2d6ef44b
NeosLab <github@neoslab.com>
2026-06-21 11:01:37 +0200

    Automated sync

--- parts/header.php
+++ parts/header.php
@@ -9,6 +9,8 @@ require('includes/recaptcha.php');
 $core = getDataByLastSegment();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
